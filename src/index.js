47ee34ea85b539c8e5b77d54897590fb35c020a8
Edwin Mwaniki <edwin.mwaniki@student.moringaschool.com>
2025-06-27 07:51:47 +0300

    Task Success: Loading items from public JSON API

---
+++ src/index.js
@@ -0,0 +1,98 @@
+let allProducts = []; // define global empty array
+// define a baseUrl string to load the json api
+let baseUrl = "https://fakestoreapi.com/products"; 
+
+// First Call to pull atleast 20 products - when the window loads
+// initial fetch declaration once the DOM is loaded
+document.addEventListener("DOMContentLoaded",() => {
+    // pulling some products from https://fakestoreapi.com/products
+    // limit the pull to 30 products
+    // https://fakestoreapi.com/products?limit=30
+    fetch(`${baseUrl}?limit=30`)
+     .then(res => res.json())
+     .then(products => {
+        allProducts = products; // assign pulled products to global array
+
+        // function to populate products on main-page
+        retrieveProducts(allProducts);
+        console.log(allProducts.length);
+
+        
+     })
+     .catch(error => (console.error("Error loading the products", error)));
+
+    
+
+
+});
+
+// function to populate products on main-page
+function retrieveProducts(products){
+    
+    console.log('The length '+products.length);
+
+    // declare some variables to used in the DOM
+    const emptyState = document.getElementById('emptyState');
+    const productsGrid = document.getElementById('products-grid');
+    const productsCount = document.getElementById('productsCount');
+    const productsOuter = document.getElementById('productsOuter');
+
+    // This is to count & display the number of posts being loaded on the page
+    productsCount.innerHTML = `<div class="flex justify-between items-center">
+                                    <span>${products.length} Product Items</span>
+                                    
+                                </div>`;
+
+     // What happens when there are no product items loaded
+
+    if(products.length === 0){
+
+        //productsOuter.innerHTML = "";
+        productsOuter.style.height = '0px';
+        emptyState.style.display = "block";
+        productsCount.textContent = "0 posts";
+    }
+    else{ // There are posts availabe for display
+       
+        emptyState.style.display = 'none';
+        console.log("We definitely have products");
+        
+        // Calls another function
+        // createPostItem function - which fills Post Title, Author & Date
+        productsGrid.innerHTML = products.map(product => createProductCard(product)).join('');
+         
+
+    }
+
+}
+
+// Create one post item
+function createProductCard(product) {
+    
+    //console.log(post.time_stamp);
+    //let dateCreated = formatToCustomDate(product.time_stamp);
+    //console.log('date after format '+typeof dateCreated, dateCreated);
+
+
+    return `
+
+        <div class="p-3  m-1  bg-white border-gray-200 border rounded-2xl">
+            <div class = "flex space-x-1">
+                <div class="m-1"><img src="${product.image}" alt="" class="object-cover w-[150px]"></div>
+                <div class="p-1">
+                    <a onclick="" class="my-2 mx-0.5 inline-block text-[18px] font-medium leading-tight cursor-pointer">${product.title}</a>
+                    <p class = "my-2 mx-0.5 font-normal block text-[14px]">
+                        ${product.description.split(" ").slice(0, 6).join(" ") +'...'}
+                    </p>
+                    <span class="inline-block"><span>${product.price}</span>
+                    </span>
+                    <span class="block">
+                        <a onclick="editPost(${product.id})" class="cursor-pointer underline text-blue-900">Add To Cart</a>
+                    
+                    </span>
+                </div>
+            </div>
+
+        </div>
+    `;
+}
